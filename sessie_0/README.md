026e1dcd0e83794c81672dccee521c7ad8766f1b
Dylan Van Assche <dylan.van.assche@protonmail.com>
2018-10-20 09:25:26 +0200

    Sessie 0 finished

--- sessie_0/README.md
+++ sessie_0/README.md
@@ -1,3 +1,16 @@
-# Sessie 0
+# Sessie 0 - OpenCV basics
+
+Maak een codeblocks project aan waarin je
+* Een command line parser maakt die meerdere afbeeldingen kan inlezen.
+* De afbeeldingen `test.png` en `testColor.png` inleest en visualiseert.
+* De kleurafbeelding opsplitst in kleurkanalen en die apart visualiseert.
+* De kleurenafbeelding omzet in een grijswaarden beeld.
+* Over de pixels van je zelfgemaakte grijswaarden afbeelding loopt en deze waardes print naar de command line.
+* Een canvas maakt en daar wat figuren op tekent (rechthoek, cirkel, ...)
+
+Let hierbij op 
+* De gemaakte afspraken
+* Het toevoegen van zoveel mogelijk controles om fouten actief op te sporen
+
 
 
--- sessie_0/main.cpp
+++ sessie_0/main.cpp
@@ -4,15 +4,16 @@
 using namespace std;
 using namespace cv;
 
-int main(int argc, const char** argv)
-{
+int main(int argc, const char** argv) {
     CommandLineParser parser(argc, argv,
-    "{ help h usage ? | | At your service! Shows this message }"
-    "{ images i | | Loads a list of images }"
+                             "{ help h usage ? | | Shows this message.}"
+                             "{ grey g         | | Loads a grey image <REQUIRED> }"
+                             "{ color c        | | Loads a color image <REQUIRED> }"
     );
 
     // Help printing
     if(parser.has("help") || argc <= 1) {
+        cerr << "Please use absolute paths when supplying your images." << endl;
         parser.printMessage();
         return 0;
     }
@@ -24,17 +25,69 @@ int main(int argc, const char** argv)
         return -1;
     }
 
-// http://answers.opencv.org/question/90232/commandlineparser-only-gets-true/
-    // Check if the user has supplied supported parameters
-    if(parser.has("images")) {
-      string image(parser.get<string>("images"));
-      if(image.empty()) {
-        parser.printMessage();
+    // Required arguments supplied?
+    string grey(parser.get<string>("grey"));
+    string color(parser.get<string>("color"));
+    if(grey.empty() || color.empty())
+    {
+        cerr << "Please supply your images using command line arguments: --grey=greyImage.png and --color=colorImage.png" << endl;
         return -1;
-      }
-      cout << image << endl;
-      //Mat image;
-      //image = imread( imageName, IMREAD_COLOR );
     }
+
+    // Try to load images
+    Mat greyImg;
+    Mat colorImg; // BGR
+    greyImg = imread(grey, IMREAD_GRAYSCALE);
+    colorImg = imread(color, IMREAD_COLOR);
+
+    if(greyImg.empty() || colorImg.empty()) {
+        cerr << "Loading images failed, please verify the paths to the images." << endl;
+        return -1;
+    }
+
+    // Displays the images in a window
+    namedWindow("Grey image", WINDOW_AUTOSIZE);
+    namedWindow("Color image", WINDOW_AUTOSIZE);
+    imshow("Grey image", greyImg);
+    imshow("Color image", colorImg);
+
+    // Splitting color channels
+    Mat splitted[3];
+    split(colorImg, splitted);
+
+    namedWindow("Color BLUE channel");
+    namedWindow("Color GREEN channel");
+    namedWindow("Color RED channel");
+    imshow("Color BLUE channel", splitted[0]);
+    imshow("Color GREEN channel", splitted[1]);
+    imshow("Color RED channel", splitted[2]);
+
+    // Transform color image to grey image using cvtColor
+    Mat colorImg2Grey;
+    cvtColor(colorImg, colorImg2Grey, COLOR_BGR2GRAY);
+    namedWindow("Color to grey");
+    imshow("Color to grey", colorImg2Grey);
+
+    // Print all pixels of our color2grey image to the command line;
+    for(int rowIndex = 0; rowIndex < colorImg2Grey.rows; rowIndex++)
+    {
+        for(int columnIndex = 0; columnIndex < colorImg2Grey.cols; columnIndex++) {
+            cout << (int)colorImg2Grey.at<uchar>(rowIndex, columnIndex); // typecasting to show it properly in the terminal
+            cout << " ";
+        }
+        cout << endl; // new line when row++
+    }
+    cout << endl;
+
+    // Canvas circle + rectangle + line drawing
+    Mat canvas = Mat::zeros(512, 512, CV_8UC3); // 512 x 512 pixel: 8 bits unsigned integer 3 channels, all black
+    circle(canvas, Point(100, 100), 20, Scalar(127, 255, 0), 1);
+    rectangle(canvas, Point(150, 150), Point(400, 300), Scalar(255, 0, 127), 10);
+    line(canvas, Point(512, 0), Point(0, 512), Scalar(255, 255, 255), 20);
+    namedWindow("Canvas", WINDOW_AUTOSIZE);
+    imshow("Canvas", canvas);
+
+    // Wait until the user decides to exit the program.
+    waitKey(0);
     return 0;
 }
