085230859469940833ae3ec279bd9cb439bc6deb
Dylan Van Assche <dylan.van.assche@protonmail.com>
2018-10-26 18:19:04 +0200

    Finished sessie 1

--- sessie_1/opdracht_2/main.cpp
+++ sessie_1/opdracht_2/main.cpp
@@ -111,13 +111,16 @@ int main(int argc, const char** argv) {
 
     // Convex hull approach -> contours
     vector< vector<Point> > contours;
+    // Find contours of a binary image.
     findContours(maskerMatrixOperation.clone(), contours, RETR_EXTERNAL, CHAIN_APPROX_NONE); // explain defines
     vector< vector<Point> > hulls;
     for(size_t i=0; i < contours.size(); i++) {
         vector<Point> hull;
+        // contour, output hull, clockwise, returnPoints -> combines contours
         convexHull(contours[i], hull);
         hulls.push_back(hull);
     }
+    // input image, contours, contourIdx (-1 = draw all contours), color, thickness (< 0, draw contour interiors), lineType, hierarchy, maxLevel, offset
     drawContours(maskerMatrixOperation, hulls, -1, 255, -1); // check docs -1
 
     Mat contouredImg(colorImg.size(), CV_8UC3);
@@ -125,13 +128,24 @@ int main(int argc, const char** argv) {
     namedWindow("Draw contours", WINDOW_AUTOSIZE);
     imshow("Draw contours", contouredImg);
     waitKey(0);
-/*
-    // Bimodal OTSU
-    namedWindow("OTSU original", WINDOW_AUTOSIZE);
-    imshow("OTSU original", );
 
     Mat threshedImg;
-    threshold(bimodalImg, threshedImg, 0, 255, CV_THRESH_BINARY | CV_THRESH_OTSU);
+    Mat colorImgGreyed;
+    Mat colorImgGreyedOriginal;
+
+    // threshold() expects a grey image
+    cvtColor(contouredImg, colorImgGreyed, COLOR_BGR2GRAY);
+    cvtColor(colorImg, colorImgGreyedOriginal, COLOR_BGR2GRAY);
+
+    // Original OTSU
+    // Fails completely due a non equal background
+    // By applying our previous optimisations to the mask, OTSU can do it's job much better
+    threshold(colorImgGreyedOriginal, threshedImg, 0, 255, CV_THRESH_BINARY | CV_THRESH_OTSU);
+    namedWindow("OTSU original thresholding", WINDOW_AUTOSIZE);
+    imshow("OTSU original thresholding", threshedImg);
+    waitKey(0);
+
+    threshold(colorImgGreyed, threshedImg, 0, 255, CV_THRESH_BINARY | CV_THRESH_OTSU);
     namedWindow("OTSU thresholding", WINDOW_AUTOSIZE);
     imshow("OTSU thresholding", threshedImg);
     waitKey(0); // Wait until the user presses a key
@@ -140,7 +154,7 @@ int main(int argc, const char** argv) {
     // 1. histogram equalisation: isn't that great either
     Mat bimodalImgEqualized;
     Mat threshedImgEqualized;
-    equalizeHist(bimodalImg, bimodalImgEqualized); // previously: .clone() to avoid losing original matrix (obselete by now)
+    equalizeHist(colorImgGreyed, bimodalImgEqualized); // previously: .clone() to avoid losing original matrix (obselete by now)
     threshold(bimodalImgEqualized, threshedImgEqualized, 0, 255, CV_THRESH_BINARY | CV_THRESH_OTSU);
     namedWindow("Equalized OTSU", WINDOW_AUTOSIZE);
     imshow("Equalized OTSU", threshedImgEqualized);
@@ -154,11 +168,11 @@ int main(int argc, const char** argv) {
     Ptr<CLAHE> clahe_p = createCLAHE();
     clahe_p->setTilesGridSize(Size(15, 15)); // window size
     clahe_p->setClipLimit(1); // 1 - 10 contrast
-    clahe_p->apply(bimodalImg.clone(), bimodalImgCLAHE); // still old C function, use .clone() to be sure
+    clahe_p->apply(colorImgGreyed.clone(), bimodalImgCLAHE); // still old C function, use .clone() to be sure
     threshold(bimodalImgCLAHE, threshedImgCLAHE, 0, 255, CV_THRESH_BINARY | CV_THRESH_OTSU);
     namedWindow("OTSU CLAHE", WINDOW_AUTOSIZE);
     imshow("OTSU CLAHE", threshedImgCLAHE);
-    waitKey(0);*/
+    waitKey(0);
 
     return 0;
 }
