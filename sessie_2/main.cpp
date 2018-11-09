8d72993e12902bfe840fe3e8de619e2b16370592
Dylan Van Assche <dylan.van.assche@protonmail.com>
2018-11-09 10:04:28 +0100

    Clean up

--- sessie_2/main.cpp
+++ sessie_2/main.cpp
@@ -104,24 +104,30 @@ int main(int argc, const char** argv) {
 
     Mat hsvSegmentedImgUpper, hsvSegmentedImgLower, hsvSegmentedImgMerged;
     Mat convertedToHSVImg;
-    low_H = 5; // Set upper and lower boundaries
-    low_S = 255;
-    low_V = 255;
-    high_H = 165;
-    high_S = 115;
-    high_V = 115;
+    const int low_H1 = 0; // Set upper and lower boundaries
+    const int low_S1 = 115;
+    const int low_V1 = 145;
+    const int low_H2 = 5; // Set upper and lower boundaries
+    const int low_S2 = 255;
+    const int low_V2 = 255;
+    const int high_H1 = 165;
+    const int high_S1 = 115;
+    const int high_V1 = 115;
+    const int high_H2 = 180;
+    const int high_S2 = 255;
+    const int high_V2 = 255;
     // Convert from BGR to HSV colorspace
     cvtColor(signImg, convertedToHSVImg, COLOR_BGR2HSV);
-    inRange(convertedToHSVImg, Scalar(0, 115, 145), Scalar(low_H, low_S, low_V), hsvSegmentedImgUpper); // thresholding on 3 channels for 170 degrees to 180 degrees (red in HSV), saturation and intensity at least 90 or higher
-    inRange(convertedToHSVImg, Scalar(high_H, high_S, high_V), Scalar(180, 255, 255), hsvSegmentedImgLower); // thresholding on 3 channels for 0 to 10 degrees (red in HSV), saturation and intensity at least 90 or higher
+    inRange(convertedToHSVImg, Scalar(low_H1, low_S1, low_V1), Scalar(low_H2, low_S2, low_V2), hsvSegmentedImgUpper); // thresholding on 3 channels for 170 degrees to 180 degrees (red in HSV), saturation and intensity at least 90 or higher
+    inRange(convertedToHSVImg, Scalar(high_H1, high_S1, high_V1), Scalar(high_H2, high_S2, high_V2), hsvSegmentedImgLower); // thresholding on 3 channels for 0 to 10 degrees (red in HSV), saturation and intensity at least 90 or higher
 
     // Merge 2 results
     addWeighted(hsvSegmentedImgUpper, 1.0, hsvSegmentedImgLower, 1.0, 0.0, hsvSegmentedImgMerged); // Same weights
 
     /*
      * HSV color space
-     * Advantages: Easy to use, open GIMP, pick the color you want to threshold, copy the BGR values and add them as boundaries
-     * Disadvantages: Shadows and other colors which are close to the one you want to filter can change the result significantly! Change the light intensity (sign.jpg and sign2.jpg: sign.jpg works but sign2.jpg is almost completely black).
+     * Advantages: Easy to segment properly a specified color using Hue, use GIMP to figure out the S and V values and convert them to OpenCV using a Python script (see folder sessie_2/gimpHSVtoOpencv.py)
+     * Disadvantages: A bit more difficult to think in this different color space, red requires 2 operations since it's laying around the border of the Hue.
      *
      * HSV is a better solution for this since you can select only the Hue of the color, in BGR is red a combination of R and a bit of B and G.
      */
@@ -142,15 +148,20 @@ int main(int argc, const char** argv) {
     vector< vector<Point> > contours;
     // Find contours of a binary image.
     findContours(hsvSegmentedImgMerged.clone(), contours, RETR_EXTERNAL, CHAIN_APPROX_NONE); // explain defines
-    vector< vector<Point> > hulls;
+    vector<Point> hull;
+    convexHull(contours[0], hull);
+    vector<Point> biggest_blob = hull;
     for(size_t i=0; i < contours.size(); i++) {
-        vector<Point> hull;
-        // contour, output hull, clockwise, returnPoints -> combines contours
-        convexHull(contours[i], hull);
-        hulls.push_back(hull);
+        if(contourArea(contours[i]) > contourArea(biggest_blob)) {
+            convexHull(contours[i], hull); // Improve contouring
+            biggest_blob = hull;
+        }
     }
+    vector < vector<Point> > temp;
+    temp.push_back(biggest_blob);
+
     // input image, contours, contourIdx (-1 = draw all contours), color, thickness (< 0, draw contour interiors), lineType, hierarchy, maxLevel, offset
-    drawContours(hsvSegmentedImgMerged, hulls, -1, 255, -1); // check docs -1
+    drawContours(hsvSegmentedImgMerged, temp, -1, 255, -1);
 
     Mat contouredImg(signImg.size(), CV_8UC3);
     signImg.copyTo(contouredImg, hsvSegmentedImgMerged);
@@ -159,8 +170,6 @@ int main(int argc, const char** argv) {
     waitKey(0);
 
     // Add trackbars to select the right threshold in an easy way
-    low_H = 0, low_S = 0, low_V = 0; // default values
-    high_H = max_value_H, high_S = max_value, high_V = max_value;
     namedWindow("Draw contours with trackbar", WINDOW_AUTOSIZE);
     createTrackbar("Low H", window_trackbar_name, &low_H, max_value_H, on_low_H_thresh_trackbar);
     createTrackbar("High H", window_trackbar_name, &high_H, max_value_H, on_high_H_thresh_trackbar);
@@ -169,39 +178,39 @@ int main(int argc, const char** argv) {
     createTrackbar("Low V", window_trackbar_name, &low_V, max_value, on_low_V_thresh_trackbar);
     createTrackbar("High V", window_trackbar_name, &high_V, max_value, on_high_V_thresh_trackbar);
     cvtColor(signImg, convertedToHSVImg, COLOR_BGR2HSV);
+    Mat hsvSegmentedImg;
 
     while (true) {
-        inRange(convertedToHSVImg, Scalar(low_H, low_S, low_V), Scalar(high_H, high_S, high_V), hsvSegmentedImgUpper); // thresholding on 3 channels for 170 degrees to 180 degrees (red in HSV), saturation and intensity at least 90 or higher
-        //inRange(convertedToHSVImg, Scalar(low_H, low_S, low_V), Scalar(high_H, high_S, high_V), hsvSegmentedImgLower); // thresholding on 3 channels for 0 to 10 degrees (red in HSV), saturation and intensity at least 90 or higher
-
-        // Merge 2 results
-        //addWeighted(hsvSegmentedImgUpper, 1.0, hsvSegmentedImgLower, 1.0, 0.0, hsvSegmentedImgMerged); // Same weights
-        hsvSegmentedImgMerged = hsvSegmentedImgUpper;
+        inRange(convertedToHSVImg, Scalar(low_H, low_S, low_V), Scalar(high_H, high_S, high_V), hsvSegmentedImg); // thresholding on 3 channels using trackbars
 
         // Connect blobs to improve segmentation
-        dilate(hsvSegmentedImgMerged, hsvSegmentedImgMerged, Mat(), Point(-1, -1), 5); // connect blobs, more times than removing noise
-        erode(hsvSegmentedImgMerged, hsvSegmentedImgMerged, Mat(), Point(-1, -1), 5); // fix dilate data loss
+        dilate(hsvSegmentedImg, hsvSegmentedImg, Mat(), Point(-1, -1), 5); // connect blobs, more times than removing noise
+        erode(hsvSegmentedImg, hsvSegmentedImg, Mat(), Point(-1, -1), 5); // fix dilate data loss
 
         // Convex hull approach -> contours
         vector< vector<Point> > contours;
         // Find contours of a binary image.
-        findContours(hsvSegmentedImgMerged.clone(), contours, RETR_EXTERNAL, CHAIN_APPROX_NONE); // explain defines
-        vector< vector<Point> > hulls;
+        findContours(hsvSegmentedImg.clone(), contours, RETR_EXTERNAL, CHAIN_APPROX_NONE); // explain defines
+        convexHull(contours[0], hull);
+        vector<Point> biggest_blob = hull;
         for(size_t i=0; i < contours.size(); i++) {
-            vector<Point> hull;
-            // contour, output hull, clockwise, returnPoints -> combines contours
-            convexHull(contours[i], hull);
-            hulls.push_back(hull);
+            if(contourArea(contours[i]) > contourArea(biggest_blob)) {
+                convexHull(contours[i], hull); // Improve contouring
+                biggest_blob = hull;
+            }
         }
+        vector < vector<Point> > temp;
+        temp.push_back(biggest_blob);
+
         // input image, contours, contourIdx (-1 = draw all contours), color, thickness (< 0, draw contour interiors), lineType, hierarchy, maxLevel, offset
-        drawContours(hsvSegmentedImgMerged, hulls, -1, 255, -1); // check docs -1
+        drawContours(hsvSegmentedImgMerged, temp, -1, 255, -1); // check docs -1
 
         Mat contouredImg(signImg.size(), CV_8UC3);
-        signImg.copyTo(contouredImg, hsvSegmentedImgMerged);
+        signImg.copyTo(contouredImg, hsvSegmentedImg);
         // Show the frames
         imshow(window_trackbar_name, contouredImg);
         char key = (char) waitKey(1);
-        if (key == 'q' || key == 27) // ESC
+        if (key == 'q' || key == 'Q' || key == 27) // ESC
         {
             break;
         }
