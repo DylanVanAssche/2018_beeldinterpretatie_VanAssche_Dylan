68e37ff8e05f72862e3edfd2df2663b1335ca361
Dylan Van Assche <dylan.van.assche@protonmail.com>
2018-11-09 09:05:51 +0100

    1st task: segmentation BGR and HSV finished

---
+++ sessie_2/gimpHSVtoOpencv.py
@@ -0,0 +1,7 @@
+gimpH = 334 
+gimpS = 58
+gimpV = 47
+openCVH = gimpH/2
+openCVS = (gimpS/100) * 255
+openCVV = (gimpV / 100) * 255
+print("H: {}\nS:{}\nV:{}\n".format(openCVH, openCVS, openCVV))
--- sessie_2/main.cpp
+++ sessie_2/main.cpp
@@ -7,8 +7,7 @@ using namespace cv;
 int main(int argc, const char** argv) {
     CommandLineParser parser(argc, argv,
                              "{ help h usage ? | | Shows this message.}"
-                             "{ grey g         | | Loads a grey image <REQUIRED> }"
-                             "{ color c        | | Loads a color image <REQUIRED> }"
+                             "{ sign s        | | Loads a color image with a traffic sign <REQUIRED> }"
     );
 
     // Help printing
@@ -26,29 +25,74 @@ int main(int argc, const char** argv) {
     }
 
     // Required arguments supplied?
-    string grey(parser.get<string>("grey"));
-    string color(parser.get<string>("color"));
-    if(grey.empty() || color.empty())
+    string sign(parser.get<string>("sign"));
+    if(sign.empty())
     {
-        cerr << "Please supply your images using command line arguments: --grey=greyImage.png and --color=colorImage.png" << endl;
+        cerr << "Please supply your images using command line arguments: --sign=sign.png" << endl;
         return -1;
     }
 
     // Try to load images
-    Mat greyImg;
-    Mat colorImg; // BGR
-    greyImg = imread(grey, IMREAD_GRAYSCALE);
-    colorImg = imread(color, IMREAD_COLOR);
+    Mat signImg;
+    signImg = imread(sign, IMREAD_COLOR);
 
-    if(greyImg.empty() || colorImg.empty()) {
+    if(signImg.empty()) {
         cerr << "Loading images failed, please verify the paths to the images." << endl;
         return -1;
     }
 
     // Displays the images in a window
-    namedWindow("Grey image", WINDOW_AUTOSIZE);
-    namedWindow("Color image", WINDOW_AUTOSIZE);
-    imshow("Grey image", greyImg);
-    imshow("Color image", colorImg);
+    namedWindow("Sign image", WINDOW_AUTOSIZE);
+    imshow("Sign image", signImg);
+
+    Mat rgbSegmentedImg;
+    const int low_B = 0; // Set upper and lower boundaries
+    const int low_G = 0;
+    const int low_R = 200;
+    const int high_B = 15;
+    const int high_G = 15;
+    const int high_R = 255;
+    inRange(signImg, Scalar(low_B, low_G, low_R), Scalar(high_B, high_G, high_R), rgbSegmentedImg); // thresholding on 3 channels
+
+    /*
+     * BGR color space
+     * Advantages: Easy to use, open GIMP, pick the color you want to threshold, copy the BGR values and add them as boundaries.
+     * Disadvantages: Shadows and other colors which are close to the one you want to filter can change the result significantly! Change the light intensity (sign.jpg and sign2.jpg: sign.jpg works but sign2.jpg is almost completely black).
+     *
+     * HSV is a better solution for this since you can select only the Hue of the color, in BGR is red a combination of R and a bit of B and G.
+     */
+
+    namedWindow("Sign segmented using BGR", WINDOW_AUTOSIZE);
+    imshow("Sign segmented using BGR", rgbSegmentedImg);
+    waitKey(0); // Wait for key input to continue
+
+    Mat hsvSegmentedImgUpper, hsvSegmentedImgLower, hsvSegmentedImgMerged;
+    Mat convertedToHSVImg;
+    const int low_H = 5; // Set upper and lower boundaries
+    const int low_S = 255;
+    const int low_V = 255;
+    const int high_H = 165;
+    const int high_S = 115;
+    const int high_V = 115;
+    // Convert from BGR to HSV colorspace
+    cvtColor(signImg, convertedToHSVImg, COLOR_BGR2HSV);
+    inRange(convertedToHSVImg, Scalar(0, 115, 145), Scalar(low_H, low_S, low_V), hsvSegmentedImgUpper); // thresholding on 3 channels for 170 degrees to 180 degrees (red in HSV), saturation and intensity at least 90 or higher
+    inRange(convertedToHSVImg, Scalar(high_H, high_S, high_V), Scalar(180, 255, 255), hsvSegmentedImgLower); // thresholding on 3 channels for 0 to 10 degrees (red in HSV), saturation and intensity at least 90 or higher
+
+    // Merge 2 results
+    addWeighted(hsvSegmentedImgUpper, 1.0, hsvSegmentedImgLower, 1.0, 0.0, hsvSegmentedImgMerged); // Same weights
+
+    /*
+     * HSV color space
+     * Advantages: Easy to use, open GIMP, pick the color you want to threshold, copy the BGR values and add them as boundaries
+     * Disadvantages: Shadows and other colors which are close to the one you want to filter can change the result significantly! Change the light intensity (sign.jpg and sign2.jpg: sign.jpg works but sign2.jpg is almost completely black).
+     *
+     * HSV is a better solution for this since you can select only the Hue of the color, in BGR is red a combination of R and a bit of B and G.
+     */
+
+    namedWindow("Sign segmented using HSV", WINDOW_AUTOSIZE);
+    imshow("Sign segmented using HSV", hsvSegmentedImgMerged);
+    waitKey(0); // Wait for key input to continue
+
     return 0;
 }
